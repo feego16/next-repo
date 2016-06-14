344d82e30c3bb8d1c15b71d73e55e728c5fc8dc1
orlando grant <feego@afeegoOrlando-MacBook.local>
2016-06-14 10:18:54 -0700

    new project

--- anotherone/ViewController.swift
+++ anotherone/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bluefish: UIImageView!
 
+    @IBOutlet weak var redfish: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeredhide(sender: AnyObject) {
+        redfish.hidden = true
+    }
 
+    @IBAction func makebluehide(sender: AnyObject) {
+        bluefish.hidden = true
+    }
+    @IBAction func makeredandblueappear(sender: AnyObject) {
+        redfish.hidden = false
+        bluefish.hidden = false
+    }
 }
 
